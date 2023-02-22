9052be0c191ec70821db9ba84f4071696c918be6
Daisy Yoo <daisyhyoo@gmail.com>
2023-02-22 02:41:00 +0000

    Added test picture for spacing

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const styles = {
-  image: {
+  backgroundImage: {
     backgroundImage: 'url("/images/eucalyptus-img-5.jpg")',
     backgroundSize: 'cover',
     backgroundPosition: '60%',
@@ -15,6 +15,11 @@ const styles = {
   //   width: '100%',
   //   height: '100%'
   // },
+  image: {
+    height: '450px',
+    objectFit: 'cover',
+    borderRadius: '10px'
+  },
   header: {
     color: '#6F8C69',
     lineHeight: '2.5rem'
@@ -22,26 +27,33 @@ const styles = {
   text: {
     color: '#6F8C69',
     fontSize: '1.1rem',
-    lineHeight: '1.5rem'
+    lineHeight: '1.8rem'
   },
   textBox: {
-    top: '40%'
+    top: '5%'
   }
 };
 
 export default function Home(props) {
   return (
     <div className="image-container" >
-      <div style={styles.image} className="position-relative">
+      <div style={styles.backgroundImage} className="position-relative">
         <div style={styles.textBox} className="container position-absolute">
-          <div className="d-flex flex-column text-center">
-            <h1 style={styles.header} className="py-2 mb-3 my-lg-4 mb-lg-5">Save the Date!</h1>
-            <p style={styles.text} className="mb-4">We are getting married!</p>
-            <p style={styles.text} className="mb-4">June 24, 2023</p>
-            <p style={styles.text} className="mb-4 px-5">Please let us know how to contact you for RSVP details.</p>
+          <div className="text-center">
+            <img style={styles.image} src="/images/test-pic.JPG" alt="joyceAndKevinPic"/>
+          </div>
+          <div className="d-flex flex-column text-center my-3">
+            <h1 style={styles.header} className="py-2 mb-3">Save the Date!</h1>
+            <p style={styles.text} className="mb-1 px-3">
+              We are getting married!
+              <br /> June 24, 2023
+              <br />More details to come!
+              <br />Please let us know how to contact you for RSVP details.
+            </p>
           </div>
         </div>
       </div>
+      <div />
     </div>
   );
 }
