87d0eae95aa28a68f00556ca8387be8f4d1657ee
Daisy Yoo <daisyhyoo@gmail.com>
2023-02-22 22:22:15 +0000

    Fixed the mobile layout and text content for better mobile viewing

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 
 const styles = {
-  backgroundImage: {
+  backgroundImage1: {
     backgroundImage: 'url("/images/eucalyptus-img-5.jpg")',
+    backgroundSize: 'cover',
+    backgroundPosition: '70%',
+    width: '100%',
+    height: '100%'
+  },
+  backgroundImage2: {
+    backgroundImage: 'url("/images/eucalyptus-img-6.jpg")',
     backgroundSize: 'cover',
     backgroundPosition: '60%',
     width: '100%',
     height: '100%'
   },
-  landingPage: {
+  pageContainer: {
     height: '95vh'
   },
   title: {
-    fontSize: '2.5rem'
+    fontSize: '5rem',
+    lineHeight: '8rem',
+    height: '100%'
   },
   image: {
     height: '450px',
@@ -20,41 +29,37 @@ const styles = {
     borderRadius: '10px'
   },
   header: {
-    color: '#6F8C69',
-    lineHeight: '2.5rem'
-  },
-  text: {
-    color: '#6F8C69',
-    fontSize: '1.5rem',
+    fontSize: '3.3rem',
     lineHeight: '3rem'
   },
-  textBox: {
-    top: '35%'
+  text: {
+    fontSize: '1.5rem',
+    lineHeight: '3rem'
   }
 };
 
 export default function Home(props) {
   return (
     <>
-      <div className="container">
-        <div style={styles.landingPage} className="d-flex flex-column justify-content-around align-items-center">
-          <h1 style={styles.title} className="py-5">Joyce &amp; Kevin</h1>
-          <img style={styles.image} src="/images/test-pic.JPG" alt="joyceAndKevinPic"/>
+      <div style={styles.pageContainer}>
+        <div style={styles.backgroundImage1}>
+          <h1 style={styles.title} className="d-flex flex-column justify-content-around align-items-center text-center py-5">Joyce <br/>&amp; <br/>Kevin</h1>
         </div>
       </div>
-      <div className="image-container" >
-        <div style={styles.backgroundImage} className="position-relative">
-          <div style={styles.textBox} className="container position-absolute">
-            <div className="d-flex flex-column text-center my-3">
-              <h1 style={styles.header} className="py-2 mb-3">Save the Date!</h1>
-              <p style={styles.text} className="mb-1 px-3">
-                We are getting married!
-                <br /> June 24, 2023
-                <br /> La Crescenta, CA
-                <br />More details to come!
-                <br />Please let us know how to contact you for RSVP details.
-              </p>
-            </div>
+      <div style={styles.pageContainer} className="d-flex flex-column justify-content-center align-items-center">
+        <img style={styles.image} src="/images/test-pic.JPG" alt="joyceAndKevinPic"/>
+        <h3 style={styles.header} className="text-center py-4 my-4">We&apos;re getting married!</h3>
+      </div>
+      <div style={styles.pageContainer}>
+        <div style={styles.backgroundImage2}>
+          <div className="d-flex flex-column text-center my-3">
+            <h1 style={styles.header} className="mt-5">Save the Date!</h1>
+            <p style={styles.text} className="p-2">
+              June 24, 2023
+              <br /> La Crescenta, CA
+              <br />More details to come!
+              <br />Please let us know how to contact you for RSVP details.
+            </p>
           </div>
         </div>
         <div />
